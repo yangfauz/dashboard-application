5a6d99de5df2e6ae05d3f8ebc2537e31dcdf468b
Yayang Fauzi Rahmatulah <yangfauz@gmail.com>
2025-04-26 10:18:44 +0700

    enhance(config): change default env api url

--- dashboard/src/utils/env.ts
+++ dashboard/src/utils/env.ts
@@ -1,5 +1,5 @@
 const ENV = {
-  BASE_API_URL: process.env.NEXT_PUBLIC_BASE_URL || "ttp://localhost:4000",
+  BASE_API_URL: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:4000",
   BASE_API_URL_SERVER: process.env.BASE_URL || "",
   NODE_ENV: process.env.NODE_ENV || "development",
   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET || "",
